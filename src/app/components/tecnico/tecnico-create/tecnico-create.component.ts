d6409675371ca1e63ebed64ae573720176175e6d
BrunoN97 <nobre.piaget@gmail.com>
2022-04-28 21:43:55 -0300

    Validações e Mascara Tecnico-create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,15 +1,24 @@
+import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tecnico-create',
   templateUrl: './tecnico-create.component.html',
-  styleUrls: ['./tecnico-create.component.scss']
+  styleUrls: ['./tecnico-create.component.scss'],
 })
 export class TecnicoCreateComponent implements OnInit {
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validaCampos() {
+    return (
+      this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+    );
   }
-
 }
