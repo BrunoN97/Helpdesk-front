87b92a1c68e3d852893f83a767a9287eb55dab75
BrunoN97 <nobre.piaget@gmail.com>
2022-05-06 15:30:33 -0300

    HTML e CSS de CHAMADO-LIST ok

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from './../../../Models/chamado';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chamado-list',
   templateUrl: './chamado-list.component.html',
-  styleUrls: ['./chamado-list.component.scss']
+  styleUrls: ['./chamado-list.component.scss'],
 })
 export class ChamadoListComponent implements OnInit {
+  ELEMENT_DATA: Chamado[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'titulo',
+    'cliente',
+    'tecnico',
+    'dataAbertura',
+    'prioridade',
+    'status',
+    'acoes',
+  ];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
